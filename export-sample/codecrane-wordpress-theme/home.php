1e5c7d6dcb5068e13bb7c680f5cffcfdee1e7790
sumi <121598930+sumicoder@users.noreply.github.com>
2024-12-21 16:24:23 +0900

    サンプルファイル更新

---
+++ export-sample/codecrane-wordpress-theme/home.php
@@ -0,0 +1,10 @@
+<?php
+/*
+* Template Name: 投稿 アーカイブページ
+*/
+?>
+
+<?php get_header(); ?>
+
+<?php get_footer(); ?>
+
---
+++ export-sample/codecrane-wordpress-theme/page-about.php
@@ -0,0 +1,10 @@
+<?php
+/*
+* Template Name: クレーンについて 固定ページ
+*/
+?>
+
+<?php get_header(); ?>
+
+<?php get_footer(); ?>
+
